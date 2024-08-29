3f1fed25815730ee74c0ef72b92819ba48966832
sun-lingyu <1758861056@qq.com>
2024-08-29 18:53:58 +0800

    update install_nvbit.sh supporting both x86_64&aarch64

--- util/tracer_nvbit/install_nvbit.sh
+++ util/tracer_nvbit/install_nvbit.sh
@@ -1,7 +1,16 @@
 export BASH_ROOT="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
-
 rm -rf $BASH_ROOT/nvbit_release
-wget https://github.com/NVlabs/NVBit/releases/download/1.7/nvbit-Linux-aarch64-1.7.tar.bz2
-tar -xf nvbit-Linux-aarch64-1.7.tar.bz2 -C $BASH_ROOT
-rm nvbit-Linux-aarch64-1.7.tar.bz2
-
+arch=$(uname -m)
+if [ $arch == "x86_64" ]
+then
+    wget https://github.com/NVlabs/NVBit/releases/download/1.7/nvbit-Linux-x86_64-1.7-1.tar.bz2
+    tar -xf nvbit-Linux-x86_64-1.7-1.tar.bz2 -C $BASH_ROOT
+    rm nvbit-Linux-x86_64-1.7-1.tar.bz2
+elif [ $arch == "aarch64" ]
+then
+    wget https://github.com/NVlabs/NVBit/releases/download/1.7/nvbit-Linux-aarch64-1.7.tar.bz2
+    tar -xf nvbit-Linux-aarch64-1.7.tar.bz2 -C $BASH_ROOT
+    rm nvbit-Linux-aarch64-1.7.tar.bz2
+else
+    echo "$arch not supported"
+fi
